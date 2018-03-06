ec3ea1c2928c878f0823b4a9adb66a23e9820240
Oleksii Borysonok <alex.borysonok@gmail.com>
2018-03-06 10:54:48 -0500

    configured express

--- app.js
+++ app.js
@@ -1,2 +1,53 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+
+
+app.use(morgan('combined'));
+
+
+
+// app.use((req, res, next) => { 
+//     //console.log("===request object:", Object.keys(req));
+//     // console.log("===req.method: ", req.method)
+//     // console.log("===req.statusCode: ", req.statusCode)
+//     // console.log("===req.URL: ", req.url);
+
+//     // morgan('', () => { 
+
+//     // })
+
+//     morgan('combined');
+
+
+
+//     next();
+// }); 
+    
+
+
+app.get('/', (req, res) => {
+    // console.log("req: ", req);
+    //console.log("res: ", res);
+
+    res.send("hello world");
+});
+
+
+app.get('/news', (req, res) => {
+    // console.log("req: ", req);
+    //console.log("res: ", res);
+
+    res.send("You reached the NEWS page! Congrats!");
+});
+
+
+
+
+
+
+
+
+app.listen(3000, function () { 
+    console.log("server is listening on port 3000: Welcome home!");
+})
