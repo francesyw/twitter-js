83b462b4735de3ec02a87dfc729085a97bbcb21b
Frances Wang <francesyuanwang@gmail.com>
2018-03-06 15:14:58 -0500

    Updated

--- app.js
+++ app.js
@@ -4,59 +4,16 @@ const app = express();
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
 
-//==========================================================//
-
-// var locals = {
-//     title: 'An Example',
-//     people: [
-//         { name: 'Gandalf'},
-//         { name: 'Frodo' },
-//         { name: 'Hermione'}
-//     ]
-// };
 
 nunjucks.configure('views', {noCache: true});
-
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
 app.use('/', routes);
-
 app.use(express.static('public'));
 
 
-
-//==========================================================//
-
-// app.get('/', (req, res) => {
-//     res.render('index.html', locals);
-// });
-
-
-// app.get('/news', (req, res) => {
-//     // console.log("req: ", req);
-//     //console.log("res: ", res);
-
-//     res.send("You reached the NEWS page! Congrats!");
-// });
-
-
-// app.get('/tweets/:id', (req, res) => {
-
-//     console.log("====req.params: ", req.params.id)
-
-
-
-//     res.render('index.html', locals);
-// });
-
-//==========================================================//
-
-
-
-
-
-app.listen(3000, function () { 
+app.listen(3000, function () {
     console.log("server is listening on port 3000: Welcome home!");
 })
--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,32 @@ const express = require('express');
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
+const bodyParser = require('body-parser');
 
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
   let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render('index', { tweets: tweets, showForm: true });
 });
 
+router.get('/users/:name', function(req, res) {
+  let nm = req.params.name;
+  var content = tweetBank.find({ name: nm });
+  res.render('index', { tweets: content, showForm: true, userName: nm });
+});
 
+router.get('/tweets/:id', function(req, res) {
+  let id = req.params.id;
+  var content = tweetBank.find({ id: Number(id) });
+  res.render('index', { tweets: content });
+});
 
-router.get('/users/:name', function (req, res) { 
-    
-    let nm = req.params.name;
-    
-
-    var content = tweetBank.find({ name: nm });
-   // console.log("==== content: ", content)
-
-    // res.render('index', { content: content });
-    res.render( 'index', { tweets: content } );
-  });
-
-
+router.post('/tweets', urlencodedParser, function (req, res, next) {
+  tweetBank.add(req.body.name, req.body.text);
+  res.redirect('/');
+})
 
 
 module.exports = router;
